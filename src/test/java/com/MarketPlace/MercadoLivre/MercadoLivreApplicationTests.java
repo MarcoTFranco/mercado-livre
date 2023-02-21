dc6e88a744d12b676aaaf4aa0c21eda21a24b02b
Marco Tulio <marcotfrancosilva@gmail.com>
2023-02-21 20:30:45 -0300

    First commit

---
+++ src/test/java/com/MarketPlace/MercadoLivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MarketPlace.MercadoLivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
